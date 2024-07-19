5f1a6e11ceb4d22ceb6d42272a17c24e7dd25aae
Retsam <chess19@gmail.com>
2024-07-19 17:34:33 -0400

    Fix vite to suggest localhost not 127.0.0.1 urls

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// @ts-ignore - don't want to install @types/node just for this line
+import * as dns from "dns";
+
+// Gets vite to suggest localhost not 127.0.0.1 urls
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
